793d9b00888b80650f786155024aa73659c3867f
Scruf <ekozitski@hotmail.com>
2016-03-06 13:30:14 -0800

    ToDoList fixed

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
+Username ek5442
+Password Nvidia980
 App located inside the HomeWork/ToDoList
 In order to run the application you will have to issue command “python manage.py runserver”
 After issuing command proceed to “127.0.0.1/ToDoList/
--- ToDoList/admin.py
+++ ToDoList/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import ToDoList
 # Register your models here.
+
+
+
+class ToDoListAdmin(admin.ModelAdmin):
+    list_description = ['description']
+    list_due_date = ['due_date']
+    list_status = ['status']
+    fieldsets =[
+        ('Description',{'fields':['list_description']}),
+        ('Due Date',{'fields':['list_due_date']}),
+        ('Status',{'fields':['status']}),
+    ]
+
+admin.site.register(ToDoList)
